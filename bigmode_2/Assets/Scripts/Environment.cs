832711b2f83fb769d124bc51c1aacda087170e39
gielheldens <gielheldens@gmail.com>
2023-12-14 21:14:18 +0100

    terrain-easier

--- bigmode_2/Assets/Scripts/Environment.cs
+++ bigmode_2/Assets/Scripts/Environment.cs
@@ -8,8 +8,8 @@ using UnityEngine.U2D;
 public class Environment : MonoBehaviour
 {
     [Header ("References")]
-    [SerializeField] private EdgeCollider2D _collider;
     [SerializeField] private SpriteShapeController _spriteShape;
+    [SerializeField] private GameObject _emptyCopy;
 
     [Header ("Attributes")]
     [SerializeField] private float _factor;
@@ -29,5 +29,35 @@ public class Environment : MonoBehaviour
         {
             child.gameObject.layer = LayerMask.NameToLayer(_layer);
         }
+
+        // points = new Vector2[_collider.pointCount];
+        // for (int i = 0; i < _collider.pointCount; i++)
+        // {
+        //     points[i] = _collider.points[i];
+        // }
+        // points[0] = UpdateFirstVertex(_collider.points[0], _collider.points[1]);
+        // points[_collider.pointCount - 1] = UpdateLastVertex(_collider.points[_collider.pointCount-2], _collider.points[_collider.pointCount-1]);
+        // _collider.points = points;
+    }
+
+    void Update()
+    {
+        //_collider.points = points;
+    }
+
+    private Vector2 UpdateFirstVertex(Vector2 p1, Vector2 p2)
+    {
+        Vector3 direction = p1 - p2;
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        Vector2 _newPoint = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _factor;
+        return p1 + _newPoint;
+    }
+
+    private Vector2 UpdateLastVertex(Vector2 p2, Vector2 p1)
+    {
+        Vector3 direction = p1 - p2;
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        Vector2 _newPoint = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _factor;
+        return p1 + _newPoint;
     }
 }
