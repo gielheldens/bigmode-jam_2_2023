2f0dd9b28738dcedfbea626df9628bbbc8f39801
gielheldens <gielheldens@gmail.com>
2023-12-14 20:51:12 +0100

    added-edge-collider

--- bigmode_2/Assets/Scripts/Environment.cs
+++ bigmode_2/Assets/Scripts/Environment.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Environment : MonoBehaviour
 {
+    [Header ("References")]
+    [SerializeField] private EdgeCollider2D _collider;
+    [SerializeField] private SpriteShapeController _spriteShape;
+
+    [Header ("Attributes")]
+    [SerializeField] private float _factor;
+
     [Header ("Layers")]
     [SerializeField] private string _layer;
+
+    Vector2[] points;
 
     void Start()
     {
